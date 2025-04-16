09fd17bf0ab1c951aeabb87f073ee3f297ce31c8
muhammadumarr <umarvaleed222@gmail.com>
2025-04-16 22:13:42 +0500

    Short Bill Work

--- src/pages/Invoice/index.ts
+++ src/pages/Invoice/index.ts
@@ -1,3 +1,6 @@
 export * from './invoice-request';
 export * from './invoice-request-detail';
 export * from './invoice-post';
+export * from './short-bill-request';
+export * from './short-bill-post';
+export * from './short-bill-detail';
